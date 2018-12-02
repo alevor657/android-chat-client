ed3baf8973d7455d4a94a023a593adfebe6f213e
Alexey <alevor657@gmail.com>
2018-12-02 22:45:06 +0100

    Created login and register activities

--- app/src/main/java/com/example/yooo/ultimatechat/MainActivity.java
+++ app/src/main/java/com/example/yooo/ultimatechat/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.yooo.ultimatechat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signin);
+    }
+
+    public void registerRedirect(View v) {
+        startActivity(new Intent(this, RegisterActivity.class));
     }
 }
