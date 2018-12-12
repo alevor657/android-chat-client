0573d7ce20cc48d1bd2358e595aaad3312eb608f
Alexey <alevor657@gmail.com>
2018-12-12 22:05:23 +0100

    Register working

--- app/src/main/java/com/example/yooo/ultimatechat/AuthAPI.java
+++ app/src/main/java/com/example/yooo/ultimatechat/AuthAPI.java
@@ -1,13 +1,10 @@
 package com.example.yooo.ultimatechat;
 
 import android.os.AsyncTask;
-import android.view.View;
-import android.widget.EditText;
 
 import com.google.gson.Gson;
 
 import java.io.IOException;
-import java.util.HashMap;
 
 import okhttp3.HttpUrl;
 import okhttp3.MediaType;
@@ -18,7 +15,11 @@ import okhttp3.Response;
 
 public class AuthAPI {
     public static final String LOGIN_ATTEMPT_FAILED = "Bad credentials, pleasy try again";
+    public static final String REGISTER_PASSWORDS_DO_NOT_MATCH = "Sorry, provided passwords did not match";
+    public static final String REGISTER_ATTEMPT_FAILED = "Sorry, this email or username is already taken or credentials format is bad";
+
     public static final HttpUrl SIGN_IN_URL = HttpUrl.parse("http://192.168.1.63:1338/user/signin");
+    public static final HttpUrl SIGN_UP_URL = HttpUrl.parse("http://192.168.1.63:1338/user/signup");
     public static final MediaType MEDIA_TYPE = MediaType.parse("application/json; charset=utf-8");
 
     public static class LoginRequest extends AsyncTask<String, Void, UserCredentials> {
@@ -28,6 +29,46 @@ public class AuthAPI {
 
             Request request = new Request.Builder()
                     .url(AuthAPI.SIGN_IN_URL)
+                    .post(body)
+                    .build();
+
+            OkHttpClient client = new OkHttpClient();
+
+            Response res = null;
+            try {
+                res = client
+                        .newCall(request)
+                        .execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            UserCredentials uc = null;
+
+            try {
+                if (res.isSuccessful()) {
+                    uc = new Gson().fromJson(res.body().string(), UserCredentials.class);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return uc;
+        }
+
+        @Override
+        protected void onPostExecute(UserCredentials result) {
+            super.onPostExecute(result);
+        }
+    }
+
+    public static class RegisterRequest extends AsyncTask<String, Void, UserCredentials> {
+        @Override
+        protected UserCredentials doInBackground(String... params) {
+            RequestBody body = RequestBody.create(AuthAPI.MEDIA_TYPE, params[0]);
+
+            Request request = new Request.Builder()
+                    .url(AuthAPI.SIGN_UP_URL)
                     .post(body)
                     .build();
 
--- app/src/main/java/com/example/yooo/ultimatechat/RegisterActivity.java
+++ app/src/main/java/com/example/yooo/ultimatechat/RegisterActivity.java
@@ -1,7 +1,15 @@
 package com.example.yooo.ultimatechat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.util.concurrent.ExecutionException;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +17,49 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void tryRegister(View v) {
+        String username = ((EditText) findViewById(R.id.username_input)).getText().toString();
+        String email = ((EditText) findViewById(R.id.email_input)).getText().toString();
+
+        String password = ((EditText) findViewById(R.id.password_input)).getText().toString();
+        String password2 = ((EditText) findViewById(R.id.password_input2)).getText().toString();
+
+        if (!password.equals(password2)) {
+            Toast.makeText(getApplicationContext(), AuthAPI.REGISTER_PASSWORDS_DO_NOT_MATCH, Toast.LENGTH_LONG).show();
+            ((EditText) findViewById(R.id.password_input)).getText().clear();
+            ((EditText) findViewById(R.id.password_input2)).getText().clear();
+            return;
+        }
+
+        UserCredentials creds = UserCredentials.getInstance();
+        creds.setUsername(username);
+        creds.setPassword(password);
+        creds.setEmail(email);
+
+        // Parse to JSON
+        String json = new Gson().toJson(creds);
+
+        AuthAPI.RegisterRequest rr = new AuthAPI.RegisterRequest();
+
+        try {
+            creds = rr.execute(json).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        if (creds == null) {
+            Toast.makeText(getApplicationContext(), AuthAPI.REGISTER_ATTEMPT_FAILED, Toast.LENGTH_LONG).show();
+        } else {
+            UserCredentials.setInstance(creds);
+        }
+
+        if (UserCredentials.getInstance().getToken() != null) {
+            Intent intent = new Intent(this, ChatListActivity.class);
+            startActivity(intent);
+        }
     }
 }
